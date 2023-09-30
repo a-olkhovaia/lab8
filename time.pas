648fb12c3d0569c7b7c07ef2a9ed328d1afe5dab
Алина Ольховая <alina.olhovaia1005@gmail.com>
2023-09-30 11:08:10 +0300

    задание 3.1

--- time.pas
+++ time.pas
@@ -1,4 +1,6 @@
 ﻿begin
   var year := ReadInteger(' год');
-  print((year mod 4 = 0) and (year mod 100 <> 0)and (year mod 400 <> 0))
+  if((year mod 4 = 0) and (year mod 100 <> 0)and (year mod 400 <> 0)) then
+    print('366')
+  else print('365')
 end.
