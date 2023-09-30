41362c5a9617b5a73aea17967eba429e19766667
Алина Ольховая <alina.olhovaia1005@gmail.com>
2023-09-30 11:12:20 +0300

    задание 3.2

---
+++ timer.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var h := ReadInteger('количество часов');
+  var m := ReadInteger('количество минут');
+  var s := ReadInteger('количество секунд ');
+  var sum := (h/120)+(m/60)+s;
+  print(sum)
+ 
+end.
